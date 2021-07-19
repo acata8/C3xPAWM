9502cb8067f46247d104096d1c78ae4988e70b78
Andrea Cataluffi <andrea.cataluffi@studenti.unicam.it>
2021-07-19 23:26:40 +0200

    fixato padding e stricky navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,3 +7,22 @@ $('.carousel').carousel({
     interval: 5000
 })
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {addStricky()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+var section = document.getElementById("section")
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+function addStricky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    section.classList.add("padding")
+  } else {
+    navbar.classList.remove("sticky")
+    section.classList.remove("padding")
+  }
+}
